179011f280bbbe4a541beace5ed33025dac7cd27
Karthi <karthikan293@gmail.com>
2016-11-27 16:56:05 +0100

    change view icon

--- app/scripts/controllers/icon.js
+++ app/scripts/controllers/icon.js
@@ -15,22 +15,24 @@ angular.module('issouFrontApp')
       'Karma'
     ];
 
-
+    this.titleIcon="Recherche icon"
     var icon="http://issou-randomizer.herokuapp.com/issou/icon/random?";
 
 
     this.randomIcon = function() {
 
       this.imgIcon=icon+timeStamp();
-
+      this.titleIcon="Recherche Random";
+      this.panelIcon="panel panel-primary"
     }
 
 
 
-    this.idIcon = '1';
+    //this.idIcon = '1';
     this.submit = function() {
-
-       this.imgIconId="http://issou-randomizer.herokuapp.com/issou/icon/"+this.idIcon;
+       this.panelIcon="panel panel-success"
+       this.imgIcon="http://issou-randomizer.herokuapp.com/issou/icon/"+this.idIcon;
+       this.titleIcon="Recherche par ID : #"+this.idIcon;
     }
 
 
